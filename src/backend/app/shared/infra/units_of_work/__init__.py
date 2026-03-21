b12602c6d64a26762a8e5fa3fcc2ad558ae82b83
yxzhin <yxzhin@gmail.com>
2026-03-21 15:23:09 +0100

    feat: implement BaseResponseDTO, InMemoryStorage, InMemoryUnitOfWork && SqlAlchemyUnitOfWork

---
+++ src/backend/app/shared/infra/units_of_work/__init__.py
@@ -0,0 +1,5 @@
+from .inm_storage import InMemoryStorage
+from .inm_uow import InMemoryUnitOfWork
+from .sqlal_uow import SqlAlchemyUnitOfWork
+
+__all__ = ["InMemoryStorage", "InMemoryUnitOfWork", "SqlAlchemyUnitOfWork"]
