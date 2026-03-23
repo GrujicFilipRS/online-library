27dc6114b71ea6540f1ae2c72e1282c0544292a2
yxzhin <yxzhin@gmail.com>
2026-03-24 00:05:22 +0100

    feat: refactor login via password; now it actually gives access, refresh && csrf tokens

--- src/backend/app/core/application/use_cases/user/login_via_password.py
+++ src/backend/app/core/application/use_cases/user/login_via_password.py
@@ -1,6 +1,5 @@
 from collections.abc import Callable
 
-from ....domain.models.user import User
 from ....domain.ports.units_of_work import BaseUserUnitOfWork
 
 
@@ -8,7 +7,10 @@ class LoginViaPasswordUseCase:
     def __init__(self, user_uow_factory: Callable[[], BaseUserUnitOfWork]):
         self.user_uow_factory = user_uow_factory
 
-    async def execute(self, username: str, password: str) -> User:
+    async def execute(self, username: str, password: str) -> tuple[str, str, str]:
         async with self.user_uow_factory() as uow:
-            user = await uow.user_service.login_user(username, password)
-            return user
+            user = await uow.user_service.get_user_by_username(username)
+            access_token, refresh_token, csrf_token = await uow.auth_service.login_user(
+                user, password
+            )
+            return access_token, refresh_token, csrf_token
