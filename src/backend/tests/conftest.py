4850c3921175ff3404e325e5718c4ca02d01ac5f
yxzhin <yxzhin@gmail.com>
2026-03-23 18:55:47 +0100

    fix: add filler values to vars in test_env

--- src/backend/tests/conftest.py
+++ src/backend/tests/conftest.py
@@ -5,13 +5,15 @@ from src.backend.app.config import get_config
 
 @pytest.fixture(autouse=True)
 def test_env(monkeypatch):
-    monkeypatch.setenv("APP_NAME", "")
-    monkeypatch.setenv("APP_DESCRIPTION", "")
-    monkeypatch.setenv("APP_VERSION", "")
-    monkeypatch.setenv("APP_SECRET_KEY", "")
-    monkeypatch.setenv("DATABASE_URL_DEV", "")
-    monkeypatch.setenv("DATABASE_URL_PROD", "")
-    monkeypatch.setenv("REDIS_URL", "")
+    monkeypatch.setenv("APP_NAME", "test73")
+    monkeypatch.setenv("APP_DESCRIPTION", "ril")
+    monkeypatch.setenv("APP_VERSION", "7.3")
+    monkeypatch.setenv(
+        "APP_SECRET_KEY", "ril73-is-a-very-secret-key-that-is-at-least-32-bytes-long"
+    )
+    monkeypatch.setenv("DATABASE_URL_DEV", "None")
+    monkeypatch.setenv("DATABASE_URL_PROD", "None")
+    monkeypatch.setenv("REDIS_URL", "None")
 
     get_config.cache_clear()
     yield
