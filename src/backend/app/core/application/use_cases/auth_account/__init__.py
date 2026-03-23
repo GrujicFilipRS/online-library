1e7cf8e19080f23c9f18fcca5f582ce1faa29fb9
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2026-03-23 17:05:51 +0100

    fix: change import schemes to actually reflect folder structure

--- src/backend/app/core/application/use_cases/auth_account/__init__.py
+++ src/backend/app/core/application/use_cases/auth_account/__init__.py
@@ -1,12 +1,12 @@
-from .autheticate import AutheticateUseCase
+from .authenticate import AutheticateUseCase
 from .change_provider_identifier import ChangeProviderIdentifierUseCase
 from .link_provider import LinkProviderUseCase
 from .list_providers import ListProvidersUseCase
 from .unlink_provider import UnlinkProviderUseCase
 
 __all__ = [
-    "LinkProviderUseCase",
     "AutheticateUseCase",
+    "LinkProviderUseCase",
     "ChangeProviderIdentifierUseCase",
     "ListProvidersUseCase",
     "UnlinkProviderUseCase",
--- src/backend/app/core/application/use_cases/auth_account/change_provider_identifier.py
+++ src/backend/app/core/application/use_cases/auth_account/change_provider_identifier.py
@@ -1,8 +1,8 @@
 from collections.abc import Callable
 from uuid import UUID
 
-from ...domain.ports.units_of_work import BaseAuthAccountUnitOfWork
-from ...domain.value_objects import AuthProvider, AuthProviderUserId
+from ....domain.ports.units_of_work import BaseAuthAccountUnitOfWork
+from ....domain.value_objects import AuthProvider, AuthProviderUserId
 
 
 class ChangeProviderIdentifierUseCase:
@@ -18,6 +18,6 @@ class ChangeProviderIdentifierUseCase:
         new_provider_user_id: AuthProviderUserId,
     ) -> None:
         async with self.auth_account_uow_factory() as uow:
-            await uow.auth_account_service.change_provider_identifier(
+            await uow.auth_account_service.change_auth_provider_identifier(
                 user_id, provider, new_provider_user_id
             )
