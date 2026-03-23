8f817e9b0f6a3dc06d963fc01dcffe4241cbe5cc
FedorFeofanov <fedor.feofanov@gmail.com>
2026-03-23 22:39:59 +0100

    fix(auth): decouple services by moving them into the use cases

--- src/backend/app/core/domain/ports/units_of_work/base_auth_account_uow.py
+++ src/backend/app/core/domain/ports/units_of_work/base_auth_account_uow.py
@@ -1,8 +1,9 @@
 from .....shared.domain.ports import BaseUnitOfWork
 from ..repositories import BaseAuthAccountRepository
-from ..services import BaseAuthAccountService
+from ..services import BaseAuthAccountService, BaseUserService
 
 
 class BaseAuthAccountUnitOfWork(BaseUnitOfWork):
     auth_account_repo: BaseAuthAccountRepository
     auth_account_service: BaseAuthAccountService
+    user_service: BaseUserService
