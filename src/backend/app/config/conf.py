3312e91a1ce9d6ac5042cac06bfc777083e332ec
yxzhin <yxzhin@gmail.com>
2026-03-23 21:24:57 +0100

    feat: refactor config && env example

--- src/backend/app/config/conf.py
+++ src/backend/app/config/conf.py
@@ -2,6 +2,7 @@
 # It checks if the required environment variables are loaded,
 # as well as init a class that's used to store environment variables
 
+from datetime import timedelta
 from functools import lru_cache
 
 from pydantic import Field, SecretStr
@@ -14,6 +15,7 @@ class Config(BaseSettings):
     APP_DESCRIPTION: str = Field(min_length=1)
     APP_VERSION: str = Field(min_length=1)
     APP_SECRET_KEY: SecretStr
+    APP_CSRF_SECRET: SecretStr
 
     DATABASE_URL_DEV: str = Field(min_length=1)
     DATABASE_URL_PROD: str = Field(min_length=1)
@@ -23,11 +25,26 @@ class Config(BaseSettings):
     PRJ_DEV_MODE: bool = True
     ENABLE_API_DOCS: bool = True
     FRONTEND_URL: str = "http://localhost:5173"
+    API_URL: str = "http://localhost:8000"
+
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 24
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+    ALGORITHM: str = "HS256"
+    COOKIE_SECURE: bool = False  # change in prod (HTTPS)
+    COOKIE_SAMESITE: str = "lax"  # strict or none (none requires SECURE)
+    COOKIE_DOMAIN: str | None = None  # e.g. "example.com" if needed
 
     @property
     def DATABASE_URL(self) -> str:  # noqa: N802
         return self.DATABASE_URL_DEV if self.PRJ_DEV_MODE else self.DATABASE_URL_PROD
+
+    @property
+    def ACCESS_TTL(self) -> timedelta:  # noqa: N802
+        return timedelta(minutes=self.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    @property
+    def REFRESH_TTL(self) -> timedelta:  # noqa: N802
+        return timedelta(days=self.REFRESH_TOKEN_EXPIRE_DAYS)
 
     model_config = SettingsConfigDict(
         env_file=".env",
