6da3388a7dcbb7b970fc95e2f5a63fb01c4f88a6
FedorFeofanov <fedor.feofanov@gmail.com>
2026-03-24 16:51:57 +0100

    feat(authors): add base classes for repository, service, uow

--- src/backend/app/core/domain/ports/repositories/__init__.py
+++ src/backend/app/core/domain/ports/repositories/__init__.py
@@ -1,4 +1,5 @@
 from .base_auth_account_repo import BaseAuthAccountRepository
+from .base_author_repo import BaseAuthorRepository
 from .base_user_repo import BaseUserRepository
 
-__all__ = ["BaseUserRepository", "BaseAuthAccountRepository"]
+__all__ = ["BaseUserRepository", "BaseAuthAccountRepository", "BaseAuthorRepository"]
