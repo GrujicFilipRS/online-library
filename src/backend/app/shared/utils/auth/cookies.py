d05e463b25b4b97035e3da0f515d675b38aa92e6
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2026-03-24 10:15:08 +0100

    refactor: extract max_age variable for less repetitivity

--- src/backend/app/shared/utils/auth/cookies.py
+++ src/backend/app/shared/utils/auth/cookies.py
@@ -10,6 +10,8 @@ class CookiesUtils:
     def set_auth_cookies(
         response: Response, access_token: str, refresh_token: str, csrf_token: str
     ) -> None:
+        max_age = int(config.ACCESS_TTL.total_seconds())
+
         response.set_cookie(
             key="access_token",
             value=access_token,
@@ -18,7 +20,7 @@ class CookiesUtils:
             samesite=config.COOKIE_SAMESITE,  # type: ignore
             domain=config.COOKIE_DOMAIN,
             path="/",
-            max_age=int(config.ACCESS_TTL.total_seconds()),
+            max_age=max_age,
         )
 
         response.set_cookie(
@@ -29,7 +31,7 @@ class CookiesUtils:
             samesite=config.COOKIE_SAMESITE,  # type: ignore
             domain=config.COOKIE_DOMAIN,
             path="/auth/refresh",
-            max_age=int(config.REFRESH_TTL.total_seconds()),
+            max_age=max_age,
         )
 
         response.set_cookie(
@@ -40,7 +42,7 @@ class CookiesUtils:
             samesite=config.COOKIE_SAMESITE,  # type: ignore
             domain=config.COOKIE_DOMAIN,
             path="/",
-            max_age=int(config.REFRESH_TTL.total_seconds()),
+            max_age=max_age,
         )
 
     @staticmethod
