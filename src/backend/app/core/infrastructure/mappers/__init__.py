5a8528d6a1cf3cf7dd1d640386e5a2f01961f3ae
FedorFeofanov <fedor.feofanov@gmail.com>
2026-03-24 17:26:00 +0100

    feat(authors): define dto; implement mappers

--- src/backend/app/core/infrastructure/mappers/__init__.py
+++ src/backend/app/core/infrastructure/mappers/__init__.py
@@ -1,4 +1,5 @@
 from .auth_account_mapper import AuthAccountMapper
+from .author_mapper import AuthorMapper
 from .user_mapper import UserMapper
 
-__all__ = ["UserMapper", "AuthAccountMapper"]
+__all__ = ["UserMapper", "AuthAccountMapper", "AuthorMapper"]
