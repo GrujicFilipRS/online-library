47254ae964114150ffe88274ea9dd22230f3049e
FedorFeofanov <fedor.feofanov@gmail.com>
2026-03-26 15:17:35 +0100

    fix(tests): fix return parameter parsing for user registering test

--- src/backend/tests/unit/application/use_cases/test_user_use_cases.py
+++ src/backend/tests/unit/application/use_cases/test_user_use_cases.py
@@ -6,9 +6,17 @@ async def test_register_and_login_via_password_use_cases(
 ):
     username = "ril737"
     password = "73"
-    user = await register_via_password.execute(username, password)
+    (
+        user,
+        login_access_token,
+        login_refresh_token,
+        login_csrf_token,
+    ) = await register_via_password.execute(username, password)
 
     assert isinstance(user, User)
+    assert isinstance(login_access_token, str)
+    assert isinstance(login_refresh_token, str)
+    assert isinstance(login_csrf_token, str)
 
     access_token, refresh_token, csrf_token = await login_via_password.execute(
         username, password
