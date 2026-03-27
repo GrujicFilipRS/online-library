53d05cb887c928229793032296da33d2c3091c96
yxzhin <yxzhin@gmail.com>
2026-03-27 17:40:29 +0100

    feat: add abstract create_tokens_for_user to base auth service

--- src/backend/app/core/domain/ports/services/base_auth_service.py
+++ src/backend/app/core/domain/ports/services/base_auth_service.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from uuid import UUID
 
 from ....domain.models import User
 from ..repositories import BaseUserRepository
@@ -6,6 +7,9 @@ from ..repositories import BaseUserRepository
 
 class BaseAuthService(ABC):
     user_repo: BaseUserRepository
+
+    @abstractmethod
+    async def create_tokens_for_user(self, user_id: UUID) -> tuple[str, str, str]: ...
 
     @abstractmethod
     async def login_user(self, user: User, password: str) -> tuple[str, str, str]: ...
