0b17e20e20db835750b6ca2fc355e2402749db2b
yxzhin <yxzhin@gmail.com>
2026-03-27 17:32:50 +0100

    feat: implement inm user auth unit of work && add it to init

--- src/backend/app/core/infrastructure/units_of_work/inmemory/__init__.py
+++ src/backend/app/core/infrastructure/units_of_work/inmemory/__init__.py
@@ -1,4 +1,9 @@
 from .inm_auth_account_uow import InMemoryAuthAccountUnitOfWork
+from .inm_user_auth_uow import InMemoryUserAuthUnitOfWork
 from .inm_user_uow import InMemoryUserUnitOfWork
 
-__all__ = ["InMemoryUserUnitOfWork", "InMemoryAuthAccountUnitOfWork"]
+__all__ = [
+    "InMemoryUserUnitOfWork",
+    "InMemoryUserAuthUnitOfWork",
+    "InMemoryAuthAccountUnitOfWork",
+]
---
+++ src/backend/app/core/infrastructure/units_of_work/inmemory/inm_user_auth_uow.py
@@ -0,0 +1,20 @@
+from .....shared.infrastructure.inm_storage import InMemoryStorage
+from .....shared.infrastructure.refresh_sess_stores import InMemoryRefreshSessionStorage
+from .....shared.infrastructure.units_of_work import InMemoryUnitOfWork
+from ....domain.ports.units_of_work import BaseUserAuthUnitOfWork
+from ...repositories.inmemory import (
+    InMemoryAuthAccountRepository,
+    InMemoryUserRepository,
+)
+from ...services import AuthAccountService, AuthService, UserService
+
+
+class InMemoryUserAuthUnitOfWork(InMemoryUnitOfWork, BaseUserAuthUnitOfWork):
+    def __init__(self, inm_storage: InMemoryStorage):
+        super().__init__(inm_storage)
+        self.user_repo = InMemoryUserRepository(self.inm_storage)
+        self.user_service = UserService(self.user_repo)
+        self.refresh_sess_store = InMemoryRefreshSessionStorage(self.inm_storage)
+        self.auth_service = AuthService(self.user_repo, self.refresh_sess_store)
+        self.auth_account_repo = InMemoryAuthAccountRepository(self.inm_storage)
+        self.auth_account_service = AuthAccountService(self.auth_account_repo)
