b410559c2ef2517de78d825b14438381ed82ab26
yxzhin <yxzhin@gmail.com>
2026-03-27 10:48:13 +0100

    feat: provide auth account repo in RepositoryProvider

--- src/backend/app/core/infrastructure/di/providers/repository_provider.py
+++ src/backend/app/core/infrastructure/di/providers/repository_provider.py
@@ -1,12 +1,23 @@
 from dishka import Provider, Scope, provide
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from ...repositories.sqlal import SqlAlchemyUserRepository
+from ...repositories.sqlal import (
+    SqlAlchemyAuthAccountRepository,
+    SqlAlchemyUserRepository,
+)
 
 
 class RepositoryProvider(Provider):
     """Provider for embedding repositories in the application."""
 
     @provide(scope=Scope.REQUEST)
-    async def user_repo(self, db_sess: AsyncSession) -> SqlAlchemyUserRepository:
+    async def provide_user_repo(
+        self, db_sess: AsyncSession
+    ) -> SqlAlchemyUserRepository:
         return SqlAlchemyUserRepository(db_sess)
+
+    @provide(scope=Scope.REQUEST)
+    async def provide_auth_account_repo(
+        self, db_sess: AsyncSession
+    ) -> SqlAlchemyAuthAccountRepository:
+        return SqlAlchemyAuthAccountRepository(db_sess)
