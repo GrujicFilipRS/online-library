e70b9e7060c9e86095750586a75576d339229005
yxzhin <yxzhin@gmail.com>
2026-03-27 17:29:05 +0100

    feat: add base user auth uow to init

--- src/backend/app/core/domain/ports/units_of_work/__init__.py
+++ src/backend/app/core/domain/ports/units_of_work/__init__.py
@@ -1,4 +1,5 @@
 from .base_auth_account_uow import BaseAuthAccountUnitOfWork
+from .base_user_auth_uow import BaseUserAuthUnitOfWork
 from .base_user_uow import BaseUserUnitOfWork
 
-__all__ = ["BaseUserUnitOfWork", "BaseAuthAccountUnitOfWork"]
+__all__ = ["BaseUserUnitOfWork", "BaseUserAuthUnitOfWork", "BaseAuthAccountUnitOfWork"]
