21d319178fe281d611d5559d50bf4d8e0bb24c00
yxzhin <yxzhin@gmail.com>
2026-03-27 11:36:13 +0100

    feat: provide all auth account use cases in UseCaseProvider

--- src/backend/app/core/infrastructure/di/providers/use_case_provider.py
+++ src/backend/app/core/infrastructure/di/providers/use_case_provider.py
@@ -2,11 +2,21 @@ from collections.abc import Callable
 
 from dishka import Provider, Scope, provide
 
+from ....application.use_cases.auth_account import (
+    AutheticateUseCase,
+    ChangeAuthProviderIdentifierUseCase,
+    LinkAuthProviderUseCase,
+    ListAuthProvidersUseCase,
+    UnlinkAuthProviderUseCase,
+)
 from ....application.use_cases.user import (
     LoginViaPasswordUseCase,
     RegisterViaPasswordUseCase,
 )
-from ...units_of_work.sqlal.sqlal_user_uow import SqlAlchemyUserUnitOfWork
+from ...units_of_work.sqlal import (
+    SqlAlchemyAuthAccountUnitOfWork,
+    SqlAlchemyUserUnitOfWork,
+)
 
 
 class UseCaseProvider(Provider):
@@ -23,3 +33,33 @@ class UseCaseProvider(Provider):
         user_uow_factory: Callable[[], SqlAlchemyUserUnitOfWork],
     ) -> LoginViaPasswordUseCase:
         return LoginViaPasswordUseCase(user_uow_factory)
+
+    @provide(scope=Scope.REQUEST)
+    async def provide_authenticate(
+        self, auth_account_uow_factory: Callable[[], SqlAlchemyAuthAccountUnitOfWork]
+    ) -> AutheticateUseCase:
+        return AutheticateUseCase(auth_account_uow_factory)
+
+    @provide(scope=Scope.REQUEST)
+    async def provide_change_auth_provider_identifier(
+        self, auth_account_uow_factory: Callable[[], SqlAlchemyAuthAccountUnitOfWork]
+    ) -> ChangeAuthProviderIdentifierUseCase:
+        return ChangeAuthProviderIdentifierUseCase(auth_account_uow_factory)
+
+    @provide(scope=Scope.REQUEST)
+    async def provide_link_auth_provider(
+        self, auth_account_uow_factory: Callable[[], SqlAlchemyAuthAccountUnitOfWork]
+    ) -> LinkAuthProviderUseCase:
+        return LinkAuthProviderUseCase(auth_account_uow_factory)
+
+    @provide(scope=Scope.REQUEST)
+    async def provide_list_auth_providers(
+        self, auth_account_uow_factory: Callable[[], SqlAlchemyAuthAccountUnitOfWork]
+    ) -> ListAuthProvidersUseCase:
+        return ListAuthProvidersUseCase(auth_account_uow_factory)
+
+    @provide(scope=Scope.REQUEST)
+    async def provide_unlink_auth_provider(
+        self, auth_account_uow_factory: Callable[[], SqlAlchemyAuthAccountUnitOfWork]
+    ) -> UnlinkAuthProviderUseCase:
+        return UnlinkAuthProviderUseCase(auth_account_uow_factory)
