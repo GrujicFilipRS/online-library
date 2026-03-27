4f8e646fd2c6cbb8029096af58fd2a3b2049f477
yxzhin <yxzhin@gmail.com>
2026-03-27 18:15:48 +0100

    feat: implement unit test for link auth provider use case

--- src/backend/tests/unit/application/use_cases/test_auth_account_use_cases.py
+++ src/backend/tests/unit/application/use_cases/test_auth_account_use_cases.py
@@ -3,6 +3,21 @@ from uuid import uuid4
 from src.backend.app.core.domain.models import AuthAccount
 
 
+async def test_link_auth_provider_use_case(register_via_password, link_auth_provider):
+    username = "ril737"
+    password = "73"
+    user = (await register_via_password.execute(username, password))[0]
+
+    provider = "github"
+    provider_user_id = uuid4().hex
+    auth_account = await link_auth_provider.execute(
+        user.user_id, provider, provider_user_id
+    )
+
+    assert isinstance(auth_account, AuthAccount)
+    assert auth_account.user_id == user.user_id
+
+
 async def test_authenticate_use_case(
     register_via_password, link_auth_provider, authenticate
 ):
