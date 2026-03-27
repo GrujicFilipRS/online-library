dbaef04f145f87166a8e4f9eb4f34171b37152b4
yxzhin <yxzhin@gmail.com>
2026-03-27 10:58:18 +0100

    feat: provide auth account uow factory in UnitOfWorkFactoryProvider

--- src/backend/app/core/infrastructure/di/providers/uow_factory_provider.py
+++ src/backend/app/core/infrastructure/di/providers/uow_factory_provider.py
@@ -3,15 +3,23 @@ from collections.abc import Callable
 from dishka import Provider, Scope, provide
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from ...units_of_work.sqlal import SqlAlchemyUserUnitOfWork
+from ...units_of_work.sqlal import (
+    SqlAlchemyAuthAccountUnitOfWork,
+    SqlAlchemyUserUnitOfWork,
+)
 
 
 class UnitOfWorkFactoryProvider(Provider):
     """Provider for implementing units of work in the application"""
 
     @provide(scope=Scope.REQUEST)
-    async def user_uow_factory(
-        self,
-        db_sess: AsyncSession,
+    async def provide_user_uow_factory(
+        self, db_sess: AsyncSession
     ) -> Callable[[], SqlAlchemyUserUnitOfWork]:
         return lambda: SqlAlchemyUserUnitOfWork(db_sess)
+
+    @provide(scope=Scope.REQUEST)
+    async def provide_auth_account_uow_factory(
+        self, db_sess: AsyncSession
+    ) -> Callable[[], SqlAlchemyAuthAccountUnitOfWork]:
+        return lambda: SqlAlchemyAuthAccountUnitOfWork(db_sess)
