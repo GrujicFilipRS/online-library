5f740a9e39067d2055fc1da3bd08edd44a348850
yxzhin <yxzhin@gmail.com>
2026-03-27 10:56:03 +0100

    feat: provide auth account service, auth service && refresh sess store in ServiceProvider

--- src/backend/app/core/infrastructure/di/providers/service_provider.py
+++ src/backend/app/core/infrastructure/di/providers/service_provider.py
@@ -1,12 +1,36 @@
 from dishka import Provider, Scope, provide
 
-from ...repositories.sqlal import SqlAlchemyUserRepository
-from ...services import UserService
+from .....shared.infra.refresh_sess_stores import RedisRefreshSessionStorage
+from ...repositories.sqlal import (
+    SqlAlchemyAuthAccountRepository,
+    SqlAlchemyUserRepository,
+)
+from ...services import AuthAccountService, AuthService, UserService
 
 
 class ServiceProvider(Provider):
     """Provider for implementing services in the application."""
 
     @provide(scope=Scope.REQUEST)
-    async def user_service(self, user_repo: SqlAlchemyUserRepository) -> UserService:
+    async def provide_user_service(
+        self, user_repo: SqlAlchemyUserRepository
+    ) -> UserService:
         return UserService(user_repo)
+
+    @provide(scope=Scope.REQUEST)
+    async def provide_refresh_sess_store(self) -> RedisRefreshSessionStorage:
+        return RedisRefreshSessionStorage()
+
+    @provide(scope=Scope.REQUEST)
+    async def provide_auth_service(
+        self,
+        user_repo: SqlAlchemyUserRepository,
+        refresh_sess_store: RedisRefreshSessionStorage,
+    ) -> AuthService:
+        return AuthService(user_repo, refresh_sess_store)
+
+    @provide(scope=Scope.REQUEST)
+    async def provide_auth_account_service(
+        self, auth_account_repo: SqlAlchemyAuthAccountRepository
+    ) -> AuthAccountService:
+        return AuthAccountService(auth_account_repo)
