3b3677155cc84f61e462043ccb965d1d6027c3b5
yxzhin <yxzhin@gmail.com>
2026-03-27 19:23:31 +0100

    feat: implement unit test for login via auth provider use case

--- src/backend/tests/unit/application/use_cases/test_user_auth_use_cases.py
+++ src/backend/tests/unit/application/use_cases/test_user_auth_use_cases.py
@@ -8,6 +8,27 @@ from src.backend.app.core.domain.exceptions import (
 )
 
 
+async def test_login_via_auth_provider_use_case_success(
+    register_via_password,
+    link_auth_provider,
+    login_via_auth_provider,
+):
+    username = "ril737"
+    password = "73"
+    user = (await register_via_password.execute(username, password))[0]
+
+    auth_provider = "google"
+    auth_provider_user_id = uuid4().hex
+    await link_auth_provider.execute(user.user_id, auth_provider, auth_provider_user_id)
+
+    access_token, refresh_token, csrf_token = await login_via_auth_provider.execute(
+        auth_provider, auth_provider_user_id
+    )
+    assert isinstance(access_token, str)
+    assert isinstance(refresh_token, str)
+    assert isinstance(csrf_token, str)
+
+
 async def test_unlink_auth_provider_use_case_success(
     register_via_password,
     link_auth_provider,
