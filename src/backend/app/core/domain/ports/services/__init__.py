2ba6e2702f387b7018d5fda0656ca9fb84fb8f2a
FedorFeofanov <fedor.feofanov@gmail.com>
2026-04-05 19:21:03 +0200

    fix(books): fix typo in import

--- src/backend/app/core/domain/ports/services/__init__.py
+++ src/backend/app/core/domain/ports/services/__init__.py
@@ -1,7 +1,7 @@
 from .base_auth_account_service import BaseAuthAccountService
 from .base_auth_service import BaseAuthService
 from .base_author_service import BaseAuthorService
-from .base_bok_service import BaseBookService
+from .base_book_service import BaseBookService
 from .base_user_service import BaseUserService
 
 __all__ = [
